96846542bc8ad2c427ee7e1a6dc3678f23cbc740
ronnahom96 <ronnahom96@gmail.com>
2023-03-16 10:05:15 +0200

    update jobmanager warpper test

--- tests/unit/clients/jobManagerWrapper.spec.ts
+++ tests/unit/clients/jobManagerWrapper.spec.ts
@@ -1,51 +1,36 @@
-import fs from 'fs';
-import config from 'config';
 import jsLogger from '@map-colonies/js-logger';
 import { ICreateTaskBody, OperationStatus } from '@map-colonies/mc-priority-queue';
 import { JobManagerWrapper } from '../../../src/clients/jobManagerWrapper';
 import { CreateJobBody, ITaskParameters } from '../../../src/common/interfaces';
-import { createJobParameters, createMetadata, createUuid } from '../../helpers/helpers';
+import { createJobParameters, createUuid } from '../../helpers/helpers';
 
 describe('jobManagerWrapper', () => {
   let jobManagerWrapper: JobManagerWrapper;
 
-  const utilitiesMock = {
-    filesToTasks: jest.fn(),
-  };
-  const jobsManagerMock = {
-    createJob: jest.fn(),
-  };
-
   beforeAll(() => {
     jobManagerWrapper = new JobManagerWrapper(jsLogger({ enabled: false }));
   });
+
   afterEach(() => {
     jest.clearAllMocks();
   });
 
-  describe('create tests', () => {
-    const queueFile = config.get<string>('ingestion.queueFile');
-
-    it(`should return the ingestion's response`, async () => {
+  describe('create', () => {
+    it(`returns a job ID and IN_PROGRESS status`, async () => {
+      // Arrange
       const modelId = createUuid();
-      const tasks: ICreateTaskBody<ITaskParameters>[] = [
-        {
-          parameters: { paths: ['a', 'b'], modelId: modelId },
-        },
-      ];
+      const tasks: ICreateTaskBody<ITaskParameters>[] = [{ parameters: { paths: ['a', 'b'], modelId: modelId }, },];
       const job: CreateJobBody = {
-        tasks: tasks,
-        resourceId: modelId,
-        version: 's',
-        type: 'bla',
-        parameters: createJobParameters(),
+        tasks: tasks, resourceId: modelId, version: 's', type: 'bla',
+        parameters: createJobParameters()
       };
-      utilitiesMock.filesToTasks.mockReturnValue(tasks);
-      jobsManagerMock.createJob.mockResolvedValue(job);
-      fs.writeFileSync(queueFile, 'a\nf', { encoding: 'utf8', flag: 'w' });
+      const expectedResponse = { jobID: "1", status: OperationStatus.IN_PROGRESS };
 
+      // Act
+      jest.spyOn(jobManagerWrapper, 'create').mockResolvedValue(expectedResponse);
       const created = await jobManagerWrapper.create(job);
 
+      // Assert
       expect(created).toHaveProperty('jobID');
       expect(created).toHaveProperty('status');
       expect(created.status).toBe(OperationStatus.IN_PROGRESS);
