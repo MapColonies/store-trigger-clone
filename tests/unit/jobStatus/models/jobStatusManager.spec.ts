208e43287debd15c8a40a787c8ecf36fb1b6abee
Ron Nachum <ronnahom96@gmail.com>
2023-03-21 14:56:26 +0200

    job manager test work

--- tests/unit/jobStatus/models/jobStatusManager.spec.ts
+++ tests/unit/jobStatus/models/jobStatusManager.spec.ts
@@ -1,59 +1,62 @@
 import jsLogger from '@map-colonies/js-logger';
 import { OperationStatus } from '@map-colonies/mc-priority-queue';
 import httpStatus from 'http-status-codes';
+import { JobManagerWrapper } from '../../../../src/clients/jobManagerWrapper';
 import { AppError } from '../../../../src/common/appError';
 import { IJobStatusResponse } from '../../../../src/common/interfaces';
 import { JobStatusManager } from '../../../../src/jobStatus/models/jobStatusManager';
+import { registerExternalValues } from '../../../helpers/testContainerConfig';
 
 let jobStatusManager: JobStatusManager;
 
 describe('jobStatusManager', () => {
-  const jobsManagerMock = {
+  const jobManagerWrapperMock = {
     getJob: jest.fn(),
-  };
+  }
+
+  beforeAll(() => {
+    registerExternalValues()
+  })
 
   beforeEach(() => {
-    // TODO: make it work with dependency injection
-    jobStatusManager = new JobStatusManager(jsLogger({ enabled: false }), jobsManagerMock as never);
-  });
-  
+    jobStatusManager = new JobStatusManager(jsLogger({ enabled: false }),
+      jobManagerWrapperMock as unknown as JobManagerWrapper);
+  })
+
   afterEach(() => {
     jest.clearAllMocks();
   });
 
   describe('#checkStatus', () => {
     it('return job status manager', async function () {
-      // Arrange
       const jobId = '123';
       const expectedResponse: IJobStatusResponse = {
         percentage: 100,
         status: OperationStatus.COMPLETED,
       };
-      jest.spyOn(jobStatusManager, "checkStatus").mockResolvedValue(expectedResponse);
 
-      // Act
-      const response = jobStatusManager.checkStatus(jobId);
+      jobManagerWrapperMock.getJob.mockResolvedValue(expectedResponse);
 
-      // Assert
-      await expect(response).resolves.toStrictEqual(expectedResponse);
+      const response = await jobStatusManager.checkStatus(jobId);
+
+      expect(response).toMatchObject(expectedResponse);
     });
 
     it(`rejects if job id doesn't exists`, async function () {
-      // Arrange
       const jobId = '123';
-      jobsManagerMock.getJob.mockResolvedValue(undefined);
 
-      // Act + Assert
+      jobManagerWrapperMock.getJob.mockResolvedValue(undefined);
+
       await expect(jobStatusManager.checkStatus(jobId)).rejects.toThrow(new AppError('', httpStatus.NOT_FOUND, 'The Job ID is not exists!', true));
     });
 
     it('rejects if jobManager fails', async () => {
-      // Arrange
       const jobId = '123';
-      jobsManagerMock.getJob.mockRejectedValue(new AppError('', httpStatus.INTERNAL_SERVER_ERROR, '', true));
+      
+      jobManagerWrapperMock.getJob.mockRejectedValue(new AppError('', httpStatus.INTERNAL_SERVER_ERROR, '', true));
 
-      // Act + Assert
       await expect(jobStatusManager.checkStatus(jobId)).rejects.toThrow(AppError);
     });
   });
 });
+
