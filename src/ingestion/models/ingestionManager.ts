1407705b99f49d55362180ff6d7713953620a276
Ron Nachum <ronnahom96@gmail.com>
2023-03-28 17:43:18 +0300

    move job manager wrapper to ingestion managaer

--- src/ingestion/models/ingestionManager.ts
+++ src/ingestion/models/ingestionManager.ts
@@ -1,29 +1,37 @@
 import { Logger } from '@map-colonies/js-logger';
-import { OperationStatus } from '@map-colonies/mc-priority-queue';
+import { ICreateTaskBody, JobManagerClient, OperationStatus } from '@map-colonies/mc-priority-queue';
 import { inject, injectable } from 'tsyringe';
 import { QueueFileHandler } from '../../handlers/queueFileHandler';
-import { JobManagerWrapper } from '../../clients/jobManagerWrapper';
 import { SERVICES } from '../../common/constants';
-import { CreateJobBody, IConfig, IProvider, IIngestionResponse, Payload } from '../../common/interfaces';
+import { CreateJobBody, IConfig, IProvider, IIngestionResponse, Payload, ITaskParameters, IJobParameters } from '../../common/interfaces';
 
 @injectable()
 export class IngestionManager {
+  private readonly providerName: string;
+  private readonly jobType: string;
+  private readonly taskType: string;
+  private readonly batchSize: number;
+  
   public constructor(
     @inject(SERVICES.LOGGER) private readonly logger: Logger,
     @inject(SERVICES.CONFIG) private readonly config: IConfig,
-    @inject(JobManagerWrapper) private readonly jobManagerClient: JobManagerWrapper,
+    @inject(JobManagerClient) private readonly jobManagerClient: JobManagerClient,
     @inject(SERVICES.PROVIDER) private readonly provider: IProvider,
-    @inject(SERVICES.QUEUE_FILE_HANDLER) protected readonly queueFileHandler: QueueFileHandler) { }
+    @inject(SERVICES.QUEUE_FILE_HANDLER) protected readonly queueFileHandler: QueueFileHandler) {
+    this.providerName = this.config.get<string>('ingestion.provider');
+    this.jobType = this.config.get<string>('worker.job.type');
+    this.batchSize = config.get<number>('worker.task.batches');
+    this.taskType = config.get<string>('worker.task.type');
+  }
 
   public async createModel(payload: Payload): Promise<IIngestionResponse> {
-    this.logger.info({ msg: 'Creating job for model', path: payload.modelPath, provider: this.config.get<string>('ingestion.provider') });
+    this.logger.info({ msg: 'Creating job for model', path: payload.modelPath, provider: this.providerName });
 
-    // eslint-disable-next-line @typescript-eslint/no-magic-numbers
-    const modelName: string = payload.modelPath.split('/').slice(-1)[0];
+    const modelName: string = this.extractModelNameFromPath(payload.modelPath);
     const createJobRequest: CreateJobBody = {
       resourceId: payload.modelId,
       version: '1',
-      type: this.config.get<string>('worker.job.type'),
+      type: this.jobType,
       parameters: { metadata: payload.metadata, modelId: payload.modelId, tilesetFilename: payload.tilesetFilename },
       productType: payload.metadata.productType,
       productName: payload.metadata.productName,
@@ -32,11 +40,12 @@ export class IngestionManager {
       status: OperationStatus.IN_PROGRESS,
       domain: '3D',
     };
-    this.logger.info({ msg: 'Starts writing content to queue file' });
     try {
+      this.logger.info({ msg: 'Starts writing content to queue file' });
       await this.provider.listFiles(modelName);
       this.logger.info({ msg: 'Finished writing content to queue file. Creating Tasks' });
-      const res: IIngestionResponse = await this.jobManagerClient.create(createJobRequest);
+
+      const res: IIngestionResponse = await this.createJob(createJobRequest);
       this.logger.info({ msg: 'Tasks created successfully' });
       this.queueFileHandler.emptyQueueFile();
       return res;
@@ -45,5 +54,55 @@ export class IngestionManager {
       this.queueFileHandler.emptyQueueFile();
       throw error;
     }
+  }
+
+  public async createJob(job: CreateJobBody): Promise<IIngestionResponse> {
+    const tasks: ICreateTaskBody<ITaskParameters>[] = this.createTasks(this.batchSize, job.resourceId);
+    job.tasks = tasks;
+
+    const jobResponse = await this.jobManagerClient.createJob<IJobParameters, ITaskParameters>(job);
+
+    const res: IIngestionResponse = {
+      jobID: jobResponse.id,
+      status: OperationStatus.IN_PROGRESS,
+    };
+
+    return res;
+  }
+
+  public createTasks(batchSize: number, modelId: string): ICreateTaskBody<ITaskParameters>[] {
+    const tasks: ICreateTaskBody<ITaskParameters>[] = [];
+    let chunk: string[] = [];
+    let data: string | null = this.queueFileHandler.readline();
+
+    while (data !== null) {
+      chunk.push(data);
+
+      if (chunk.length === batchSize) {
+        const task = this.buildTaskFromChunk(chunk, modelId);
+        tasks.push(task);
+        chunk = [];
+      }
+
+      data = this.queueFileHandler.readline();
+    }
+
+    // Create task from the rest of the last chunk
+    if (chunk.length > 0) {
+      const task = this.buildTaskFromChunk(chunk, modelId);
+      tasks.push(task);
+    }
+
+    return tasks;
+  }
+
+  private buildTaskFromChunk(chunk: string[], modelId: string): ICreateTaskBody<ITaskParameters> {
+    const parameters: ITaskParameters = { paths: chunk, modelId };
+    return { type: this.taskType, parameters };
+  }
+
+  private extractModelNameFromPath(modelPath: string): string {
+    // eslint-disable-next-line @typescript-eslint/no-magic-numbers
+    return modelPath.split('/').slice(-1)[0];
   }
 }
