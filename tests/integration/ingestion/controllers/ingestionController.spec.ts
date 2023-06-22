39e73c625814d8b4a37de161182e6a0886b4f6b5
Ron Nachum <ronnahom96@gmail.com>
2023-06-22 14:47:02 +0300

    chore: run comments and fix lint

--- tests/integration/ingestion/controllers/ingestionController.spec.ts
+++ tests/integration/ingestion/controllers/ingestionController.spec.ts
@@ -13,9 +13,6 @@ import { IngestionRequestSender } from '../helpers/requestSender';
 
 describe('IngestionController', function () {
   let requestSender: IngestionRequestSender;
-  // let ingestionManager: IngestionManager;
-  const s3Config = config.get<string>('S3');
-
   const jobManagerClientMock = {
     createJob: jest.fn(),
   };
@@ -36,7 +33,6 @@ describe('IngestionController', function () {
       ],
     });
 
-    // ingestionManager = container.resolve(IngestionManager);
     requestSender = new IngestionRequestSender(app);
   });
 
@@ -49,7 +45,6 @@ describe('IngestionController', function () {
     describe('Happy Path 🙂', function () {
       it('should return 201 status code and the added model', async function () {
         const payload = createPayload('model1');
-        // s3Mock.on(ListObjectsCommand).resolves(s3Output);
         jobManagerClientMock.createJob.mockResolvedValueOnce({ id: '1' });
 
         const response = await requestSender.create(payload);
@@ -59,18 +54,6 @@ describe('IngestionController', function () {
         expect(response.body).toHaveProperty('status', OperationStatus.IN_PROGRESS);
       });
     });
-
-    // describe('Bad Path 😡', function () {
-    //   it(`should return 400 status code and error message if model doesn't exists`, async function () {
-    //     const invalidPayload = createPayload('bla');
-    //     s3Mock.on(ListObjectsCommand).resolves(s3EmptyOutput);
-
-    //     const response = await requestSender.create(invalidPayload);
-
-    //     expect(response.status).toBe(httpStatusCodes.BAD_REQUEST);
-    //     expect(response.body).toHaveProperty('message', `Model bla doesn't exists in bucket ${bucket}!`);
-    //   });
-    // });
 
     describe('Sad Path 😥', function () {
       it('should return 500 status code if a network exception happens in job manager', async function () {
@@ -83,97 +66,6 @@ describe('IngestionController', function () {
         expect(response.status).toBe(httpStatusCodes.INTERNAL_SERVER_ERROR);
         expect(response.body).toHaveProperty('message', 'JobManager is not available');
       });
-
-      // it('should return 500 status code if a network exception happens in amazon s3', async function () {
-      //   const payload = createPayload('bla');
-      //   s3Mock.on(ListObjectsCommand).rejects();
-
-      //   const response = await requestSender.create(payload);
-
-      //   expect(response.status).toBe(httpStatusCodes.INTERNAL_SERVER_ERROR);
-      //   expect(response.body).toHaveProperty('message', `Didn't throw a S3 exception in file`);
-      // });
     });
   });
-
-  // describe('POST /ingestion on NFS', function () {
-  //   const nfsConfig = config.get<string>('NFS');
-
-  //   beforeAll(function () {
-  //     const app = getApp({
-  //       override: [
-  //         { token: SERVICES.JOB_MANAGER_CLIENT, provider: { useValue: jobManagerClientMock } },
-  //         { token: SERVICES.PROVIDER_CONFIG, provider: { useValue: nfsConfig } },
-  //         {
-  //           token: SERVICES.PROVIDER,
-  //           provider: {
-  //             useFactory: (): IProvider => {
-  //               return getProvider('nfs');
-  //             },
-  //           },
-  //         },
-  //       ],
-  //     });
-
-  //     requestSender = new IngestionRequestSender(app);
-  //   });
-
-  //   describe('Happy Path 🙂', function () {
-  //     it('should return 201 status code and the added model', async function () {
-  //       const payload = createPayload('model1');
-  //       jobManagerClientMock.createJob.mockResolvedValue({ id: '123', status: OperationStatus.IN_PROGRESS });
-
-  //       const response = await requestSender.create(payload);
-
-  //       expect(response.status).toBe(httpStatusCodes.CREATED);
-  //       expect(response.body).toHaveProperty('jobID', '123');
-  //       expect(response.body).toHaveProperty('status', OperationStatus.IN_PROGRESS);
-  //     });
-  //   });
-
-  //   describe('Bad Path 😡', function () {
-  //     it(`should return 400 status code and error message if model doesn't exists`, async function () {
-  //       const invalidPayload = createPayload('bla');
-
-  //       const response = await requestSender.create(invalidPayload);
-
-  //       expect(response.status).toBe(httpStatusCodes.BAD_REQUEST);
-  //       expect(response.body).toHaveProperty('message', `Model bla doesn't exists in the agreed folder`);
-  //     });
-  //   });
-
-  //   describe('Sad Path 😥', function () {
-  //     it('should return 500 status code if a network exception happens in job service', async function () {
-  //       const invalidPayload = createPayload('model1');
-  //       jobManagerClientMock.createJob.mockRejectedValueOnce(new Error('JobManager is not available'));
-
-  //       const response = await requestSender.create(invalidPayload);
-
-  //       expect(response.status).toBe(httpStatusCodes.INTERNAL_SERVER_ERROR);
-  //       expect(response.body).toHaveProperty('message', 'JobManager is not available');
-  //     });
-  //   });
-  // });
-
-  // describe('create', () => {
-  //   it('should create a new job and return it in the response', async () => {
-  //     const payload = createPayload('my-model');
-  //     ingestionManager.createJob(ingestionResponseMock);
-
-  //     const response = await requestSender.create(payload);
-
-  //     expect(response.status).toBe(201);
-  //     expect(response.body).toHaveProperty('jobID');
-  //   });
-
-  //   it('should return an error if there is an AppError', async () => {
-  //     const payload = createPayload('my-model');
-  //     managerMock.createJob.mockRejectedValue(new AppError(httpStatus.INTERNAL_SERVER_ERROR, 'some error message', true));
-
-  //     const response = await requestSender.create(payload);
-
-  //     expect(response.status).toBe(500);
-  //     expect(response.body).toHaveProperty('message', 'some error message');
-  //   });
-  // });
 });
