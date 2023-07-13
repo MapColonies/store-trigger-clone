3b665848f6e2422e03423148b8b6a06efa365c51
netanelC <netanel153@gmail.com>
2023-07-13 11:53:15 +0300

    fix: job type to be from config

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,3 +1,4 @@
+import config from 'config';
 import { readPackageJsonSync } from '@map-colonies/read-pkg';
 
 export const SERVICE_NAME = readPackageJsonSync().name ?? 'unknown_service';
@@ -6,7 +7,7 @@ export const DEFAULT_SERVER_PORT = 80;
 export const IGNORED_OUTGOING_TRACE_ROUTES = [/^.*\/v1\/metrics.*$/];
 export const IGNORED_INCOMING_TRACE_ROUTES = [/^.*\/docs.*$/];
 
-export const JOB_TYPE = 'ingestion';
+export const JOB_TYPE = config.get<string>('fileSyncer.job.type');
 
 /* eslint-disable @typescript-eslint/naming-convention */
 export const SERVICES: Record<string, symbol> = {
